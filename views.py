29bc802133e09e03cb10fbafbbe7f4b21581e303
Xamidullo_dev <xamidullo2105@gmail.com>
2025-06-11 16:25:54 +0500

    register_login

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from utils import send_email
-from file_manager import append
+from file_manager import append, read, writerows
+import random
 
 
 def register():
@@ -13,14 +14,61 @@ def register():
         password1 = input("Enter your password: ")
         password2 = input("Enter your password again: ")
 
-    send_email(receiver_email=email, body="0000")
+    otp = random.randint(1000, 9999)
+
+    send_email(receiver_email=email, body=str(otp))
     append(filename="users",data=[full_name, email,password1])
+    return check_otp(otp, email)
 
 
 def login():
-    pass
+    email = input("Enter your email: ")
+    password = input("Enter your password: ")
+    
+    users = read("users")
+    for row in users:
+        if len(row) < 3:
+            continue
+        
+        full_name = row[0].strip()
+        user_email = row[1].strip()
+        user_password = row[2].strip()
+        
+        if email == user_email and password == user_password:
+            print(f"Login seccessful ✅. Welcome {full_name}")
+            return True
+
+    print("Login failed. Invalid email or password ❌")
+    return False
 
 
+def logout(filename="users"):
+    users = read(filename)
+    updated = []
+    for row in users:
+        if len(row) >= 5:
+            row[4] = "False"
+        updated.append(row)
+        
+    writerows(filename, updated)
+    print("All users have been logged out ✅")
 
-def logout():
-    pass
+
+def check_otp(expected_code, email, filename="users"):
+    code = input("Enter the code: ")
+    success = code == str(expected_code)
+    
+    users = read(filename="users")
+    updated = []
+    
+    for row in users:
+        if len(row) >= 3 and row[1] == email:
+            if success:
+                updated.append(row + ["True"])
+            else:
+                updated.append(row + ["False"])
+        else:
+            updated.append(row)
+            
+    writerows(filename, updated)
+    return success
